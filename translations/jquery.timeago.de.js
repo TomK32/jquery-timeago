d3442889385bc9c402543ee3081b1850c8b0dd4f
Thomas R. Koll <tomk32@tomk32.de>
2011-02-03 11:00:45 +0100

    Add German translation

---
+++ translations/jquery.timeago.de.js
@@ -0,0 +1,22 @@
+
+/* German translation.
+ * Copyright (c) 2011, Thomas R. Koll (info@ananasblau.com)
+ */
+$.timeago.settings.strings = {
+  prefixAgo: "vor",
+  prefixFromNow: "in",
+  suffixAgo: null,
+  suffixFromNow: null,
+  seconds: "weniger als eine Minute",
+  minute: "etwa einer Minute",
+  minutes: "%d Minuten",
+  hour: "etwa einer Stunde",
+  hours: "etwa %d Stunden",
+  day: "ein Tag",
+  days: "%d Tagen",
+  month: "etwa ein Monat",
+  months: "%d Monaten",
+  year: "etwa ein Jahr",
+  years: "%d Jahren",
+  numbers: []
+};
